5cb2a937840de411a9ec2774b47a08a86333ff09
Rosee Feliz <rossefeliz022@gmail.com>
2020-09-10 22:24:31 -0400

    Terminando las practicas

---
+++ Practica2/Practice2.py
@@ -0,0 +1,66 @@
+#1
+# a= int(input("Ingrese el numero que desee: "))
+# b= []
+
+# while a != 0:
+#     b.append(a)
+#     a= int(input("Ingrese el numero que desee: "))
+
+
+# print("Ha marcado 0", "sus intentos fueron", b ,"la suma total es: ", sum(b))
+    
+#2
+# def Menu():
+#     print ("Bienvenido al Menu: 1. Convertir de Celsius a Fahrenheit, 2. Convertir de dolar a pesos, 3. Convertir metros a pies, 4. Salir")
+
+
+# Menu()
+# n = int(input("Ingrese el numero de la opcion deseada: "))
+
+# while n != 4:
+#     if n == 1:
+#         x= input("Ingrese el grado: ")
+#         print((int(x) * 9/5) + 32)
+#     elif n == 2:
+#         y= input("Ingrese el monto: ")
+#         print(int(y) * 58.44)
+#     else:
+#         n == 3
+#         z= input("Ingrese la medida: ")
+#         print(int(z) * 3.281)
+
+# Menu()
+# print(n)
+
+# print("Gracias por usar mi programa")
+
+#3
+n= int(input("Ingrese el numero que desee"))
+d=5
+while  d <= 1000:
+    print(n * d)
+    d += 5
+
+
+#4
+sueldo= float(input("Ingrese el sueldo: "))
+isr=
+ars=
+afp=
+
+
+
+#5
+
+    
+
+
+
+
+
+
+
+
+
+
+
