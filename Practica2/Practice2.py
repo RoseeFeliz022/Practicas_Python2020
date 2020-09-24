0707ca72603054ca37a0e315a891ff6caed6a69c
Rosee Feliz <rossefeliz022@gmail.com>
2020-09-23 23:20:47 -0400

    terminando practica 3

--- Practica2/Practice2.py
+++ Practica2/Practice2.py
@@ -1,13 +1,13 @@
 #1
-# a= int(input("Ingrese el numero que desee: "))
-# b= []
+# numero = 1
+# cantidad = 0
+# suma = 0
+# while numero != 0:
+#     numero = int(input("Ingrese el numero que desee: "))
+#     suma = suma + numero
+#     cantidad = cantidad + 1
 
-# while a != 0:
-#     b.append(a)
-#     a= int(input("Ingrese el numero que desee: "))
-
-
-# print("Ha marcado 0", "sus intentos fueron", b ,"la suma total es: ", sum(b))
+# print(f"Ha marcado 0, sus intentos fueron {cantidad} la suma total es: {suma}")
     
 #2
 # def Menu():
@@ -16,41 +16,122 @@
 
 # Menu()
 # n = int(input("Ingrese el numero de la opcion deseada: "))
-
-# while n != 4:
+# while n < 4:
 #     if n == 1:
 #         x= input("Ingrese el grado: ")
 #         print((int(x) * 9/5) + 32)
+#         Menu()
+#         n = int(input("Ingrese el numero de la opcion deseada: "))
 #     elif n == 2:
 #         y= input("Ingrese el monto: ")
 #         print(int(y) * 58.44)
+#         Menu()
+#         n = int(input("Ingrese el numero de la opcion deseada: "))
 #     else:
 #         n == 3
 #         z= input("Ingrese la medida: ")
 #         print(int(z) * 3.281)
-
-# Menu()
-# print(n)
+#         Menu()
+#         n = int(input("Ingrese el numero de la opcion deseada: ")) 
 
 # print("Gracias por usar mi programa")
 
 #3
-n= int(input("Ingrese el numero que desee"))
-d=5
-while  d <= 1000:
-    print(n * d)
-    d += 5
+# n= int(input("Ingrese el numero que desee"))
+# d=5
+# while  d <= 1000:
+#     print(n * d)
+#     d += 5
 
 
 #4
-sueldo= float(input("Ingrese el sueldo: "))
-isr=
-ars=
-afp=
+# sueldo= int(input("Ingrese el sueldo: "))
+# top1 = 416202.00
+# top2 = 624329.00
+# top3 = 867123.00
+# sueldo_anual= sueldo * 12
+# isr = 0;
 
+# if sueldo_anual <= top1:
+#     print ("Excento")
+# elif sueldo_anual <= top2:
+#     excedente = sueldo_anual - top1
+#     isr = excedente * 0.15
+# elif sueldo_anual <= top3:
+#     excedente = sueldo_anual - top2
+#     isr= 31216.00 + (excedente * 0.20)
+# else:
+#     excedente = sueldo_anual - top3
+#     isr = 79776.00 + (excedente * 0.25)
 
+# ars= sueldo * 0.0304
+# afp= sueldo * 0.0287
+ 
+# print(f"Se descuenta a su sueldo anual {sueldo_anual}: un isr de {isr}, de la ars es de {ars*12} y de la afp es de {afp*12}")
 
 #5
+
+
+carga1000 = 9000
+carga500 = 9500
+carga100 = 9900
+     
+def sacar_dinero(cantidad):
+    global carga1000, carga500, carga100
+    if cantidad <= 10000:
+        de1000 = int(cantidad / 1000)
+        cantidad = cantidad % 1000
+    if de1000 >= carga1000: # Si hay suficientes billetes de 50
+        cantidad = cantidad + (de1000 - carga1000) * 1000
+        de1000 = carga1000
+ 
+    de500 = int(cantidad / 500)
+    cantidad = cantidad % 500
+    if de500 >= carga500: # y hay suficientes billetes de 20
+       cantidad = cantidad + (de500 - carga500) * 500
+       de500 = carga500
+ 
+    de100 = int(cantidad / 100)
+    cantidad = cantidad % 100
+    if de100 >= carga100: # y hay suficientes billetes de 10
+       cantidad = cantidad + (de100 - carga100) * 100
+       de100 = carga100
+ 
+    # Si todo ha ido bien, la cantidad que resta por entregar es nula:
+    if cantidad == 0:
+      # Así que hacemos efectiva la extracción
+      carga1000 = carga1000 - de1000
+      carga500 = carga500 - de500
+      carga100 = carga100 - de100
+      return [de1000, de500, de100]
+    else: # Y si no, devolvemos la lista con tres ceros:
+      return [0, 0, 0]
+  else:
+    return [-1, -1, -1]
+ 
+ def saludar():
+     print("Bienvenido al cajero automatico del Banco ABC")
+
+
+try:
+    saludar()
+    banco = int(input("Ingrese el nombre de su banco: 1. Banco ABC, 2. Otros"))
+    servicio = int(input("Ingrese el numero de la opcion deseada: 1. Retiro, 2. Transferencia"))
+    c = int(input('Ingrese el monto a retirar: '))
+    resultado=sacar_dinero(c)
+    if resultado==[0,0,0]:
+        print ('No hay desglose de billetes para su importe')
+    elif resultado==[-1,-1,-1]:
+        print ('No hay suficientes billetes')
+    else:
+        print ('Billetes de 1000:', resultado[0])
+        print ('Billetes de 500:', resultado[1])
+        print ('Billetes de 100:', resultado[2])
+except:
+    print ('Importe incorrecto')
+
+         
+
 
     
 
---
+++ Practice3/Practica3.py
@@ -0,0 +1,82 @@
+#1
+# def elevar(x, y):
+#     print (x**y)
+
+# elevar(2, 3)
+# #2
+# x= int(input("Ingrese un numero del 1 al 10: "))
+# if  x == 1:
+#     print ("A")
+# elif x == 2:
+#     print ("B")
+# elif x == 3:
+#     print ("C")
+# elif x == 4:
+#     print ("D")
+# elif x == 5:
+#     print ("E")
+# elif x == 6:
+#     print ("F")
+# elif x == 7:
+#     print ("G")
+# elif x == 8:
+#     print ("H")
+# elif x == 9:
+#     print ("I")
+# elif x == 10:
+#     print ("J")
+# else:
+#     print ("Debe ingresar un numero del 1 al 10")
+
+# print("Gracias por utilizar mi programa")
+# #3
+# def tipo_año (año):
+#     if año %4 == 0:
+#         print("True")
+#     else:
+#         print ("False")
+
+
+# tipo_año(1992)
+#4
+#def evaluar (x, y):
+#    if x == y:
+#        print("True")
+#    else:
+#        print("False")
+
+
+#evaluar (3, 4)
+#5
+# def numPalindromo():
+#     for a in range ((999 * 999), (100 * 100), -1):
+#         if str(a) == str(a)[::-1]:
+#             print (a)
+#             break
+
+
+# numPalindromo()
+#6
+# def validarCed (x):
+#     if x < 100000000000:
+#         print("Valido")
+#     else:
+#         print ("Invalido")
+
+
+# validarCed(40127212225)
+#7
+# numero = 1
+# while numero != 0:
+#     numero = int(input("Ingrese el numero que desee: "))
+#     duplicar = numero * 2
+#     print (duplicar)
+
+#8
+
+def valor (inicial, final):
+    for x
+
+        
+
+        
